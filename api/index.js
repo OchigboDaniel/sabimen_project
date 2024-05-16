046680386dcf148695df1cb9592d4442b2335812
unknown <ochigbodaniel240@gmail.com>
2024-05-16 14:20:12 -0700

    added body parser

--- api/index.js
+++ api/index.js
@@ -6,13 +6,20 @@ const app = express()
 
 app.use(express.json())
 
+app.use(body_parser.json())
+
 app.get('/api/', (req, res) => {
     res.send("Hello World")
 })
 
 app.post(
     '/api/package', async(req, res) => {
-       res.send("post request")
+       try {
+        const dstvgotvSubscription = await DStvGOtvSubscription.create(req.body)
+        res.status(200).json(dstvgotvSubscription)
+    }catch(error){
+        res.status(500).json({ message: message.error})
+    }
     }
 )
 
