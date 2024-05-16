8c55189bb52c0abfe0fae3553e482eedd3caf2c3
unknown <ochigbodaniel240@gmail.com>
2024-05-16 14:05:20 -0700

    recentered the post request

--- api/index.js
+++ api/index.js
@@ -12,12 +12,7 @@ app.get('/api/', (req, res) => {
 
 app.post(
     '/api/package', async(req, res) => {
-       try {
-        const dstvgotvSubscription = await DStvGOtvSubscription.create(req.body)
-        res.status(200).json(dstvgotvSubscription)
-    }catch(error){
-        res.status(500).json({ message: message.error})
-    }
+       res.send("post request")
     }
 )
 
