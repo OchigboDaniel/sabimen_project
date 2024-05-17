96396d9872cc0cebd724e3506fe174c3fca07c4f
unknown <ochigbodaniel240@gmail.com>
2024-05-17 15:39:25 -0700

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/router');
+const PORT = process.env.PORT || 3000;
+
+app.use("/", routes);
+
+app.listen(
+    PORT, function (){
+        console.log("Server started at http://localhost:3000")
+    }
+);
 
 module.exports = app;
