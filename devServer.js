f58eefc0c8ca7517679c516bdf5e75a98aaa6109
unknown <ochigbodaniel240@gmail.com>
2024-05-17 06:51:15 -0700

    edited the port

--- devServer.js
+++ devServer.js
@@ -1,10 +1,10 @@
 const app = require('./app');
 const routes = require('./routes/router');
-
+const PORT = process.env.PORT || 3000;
 app.use("/", routes);
 
 app.listen(
-    3000, function (){
+    PORT, function (){
         console.log("Server started at http://localhost:3000")
     }
 );
